df0362981b9c12c083665454739e80c8ec4a6cda
Weinan Li <l.weinan@gmail.com>
2019-01-10 13:02:30 +0800

    .

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,7 +16,8 @@ sed s/HOSTNAME/$HOSTNAME/ /usr/local/hadoop/etc/hadoop/core-site.xml.template > 
 /usr/sbin/sshd -D &
 $HADOOP_PREFIX/sbin/start-dfs.sh
 $HADOOP_PREFIX/sbin/start-yarn.sh
-$HADOOP_PREFIX/sbin/mr-jobhistory-daemon.sh start historyserver
+# https://unix.stackexchange.com/questions/76354/who-sets-user-and-username-environment-variables/76356
+USER=`whoami` $HADOOP_PREFIX/sbin/mr-jobhistory-daemon.sh start historyserver
 
 if [[ $1 == "-d" ]]; then
   while true; do sleep 1000; done
